a4233cf3c7ec87961eedf0689c42870ef3f879a1
Jesus Salcedo <sjsalcedoa@unal.edu.co>
2019-07-09 00:18:58 -0500

    change port again

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 //const routes = require('./routes/index')
 
 // Settings 
-app.set('port',process.env.PORT || 3000);
+app.set('port',process.env.PORT || 3003);
 //app.set('views', path.join(__dirname,'views'));
 // ejs motor de plantillas
 //app.set('view engine', 'ejs');
